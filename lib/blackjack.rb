cd34377df632da6c72a3d8ed2731004d05f27bb4
Princeton Rose <decoyzpr@gmail.com>
2019-03-02 20:42:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,9 +65,9 @@ def runner
   # code runner here
   welcome
   card_total = initial_round
-  until card_total > 21
-    hit?
-    display_card_total
+  while card_total <= 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
-  end_game
+  end_game(card_total)
 end
