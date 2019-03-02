6a7a2c5e4012980794b7c615fb3ff7dcd5665f32
Princeton Rose <decoyzpr@gmail.com>
2019-03-02 19:58:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,9 +30,10 @@ end
 
 def initial_round
   # code #initial_round here
-  deal_card
-  deal_card
-  display_card_total
+  card_total = 0
+  card_total += deal_card
+  card_total += deal_card
+  display_card_total(card_total)
 
 end
 
